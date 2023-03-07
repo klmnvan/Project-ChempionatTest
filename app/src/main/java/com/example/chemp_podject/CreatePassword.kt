40c36aa070c4f01dcb2e47d05fa52a18ff9643cd
klmnvan <126891264+klmnvan@users.noreply.github.com>
2023-03-07 23:06:16 +0300

    Попытка наладить окно ввода пароля2
    Уже кружки нормально заполняются, но при уменьшении индекса (нажатие на стереть) из переменной password не стираются символы

--- app/src/main/java/com/example/chemp_podject/CreatePassword.kt
+++ app/src/main/java/com/example/chemp_podject/CreatePassword.kt
@@ -13,6 +13,7 @@ class CreatePassword : AppCompatActivity() {
         setContentView(R.layout.activity_create_password)
         binding = ActivityCreatePasswordBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
+        init()
     }
     private fun init()
     {
@@ -21,54 +22,64 @@ class CreatePassword : AppCompatActivity() {
             buttonCircle1.setOnClickListener() {
                 password += buttonCircle1.text
                 index++
-                SetPassword()
+                SetPassword(index)
             }
             buttonCircle2.setOnClickListener() {
                 password += buttonCircle2.text
                 index++
+                SetPassword(index)
             }
             buttonCircle3.setOnClickListener() {
                 password += buttonCircle3.text
                 index++
+                SetPassword(index)
             }
             buttonCircle4.setOnClickListener() {
                 password += buttonCircle4.text
                 index++
+                SetPassword(index)
             }
             buttonCircle5.setOnClickListener() {
                 password += buttonCircle5.text
                 index++
+                SetPassword(index)
             }
             buttonCircle6.setOnClickListener() {
                 password += buttonCircle6.text
                 index++
+                SetPassword(index)
             }
             buttonCircle7.setOnClickListener() {
                 password += buttonCircle7.text
                 index++
+                SetPassword(index)
             }
             buttonCircle8.setOnClickListener() {
                 password += buttonCircle8.text
                 index++
+                SetPassword(index)
             }
             buttonCircle9.setOnClickListener() {
                 password += buttonCircle9.text
                 index++
+                SetPassword(index)
             }
             buttonCircle0.setOnClickListener() {
                 password += buttonCircle0.text
                 index++
+                SetPassword(index)
             }
             buttonClean.setOnClickListener(){
                 //password = password.replace() //Как удалить символ из строки???
                 index--
+                SetPassword(index)
             }
         }
     }
 
-    private fun SetPassword(){
+    private fun SetPassword(point: Int){
         with(binding!!){
-            when(index){
+            when(point){
                 0 -> {
                     point1.background = getDrawable(R.drawable.point_style_blue)
                     point2.background = getDrawable(R.drawable.point_style_stroke)
@@ -76,21 +87,21 @@ class CreatePassword : AppCompatActivity() {
                     point4.background = getDrawable(R.drawable.point_style_stroke)
                 }
                 1 -> {
-                    point1.background = getDrawable(R.drawable.point_style_stroke)
+                    point1.background = getDrawable(R.drawable.point_style_blue)
                     point2.background = getDrawable(R.drawable.point_style_blue)
                     point3.background = getDrawable(R.drawable.point_style_stroke)
                     point4.background = getDrawable(R.drawable.point_style_stroke)
                 }
                 3 -> {
-                    point1.background = getDrawable(R.drawable.point_style_stroke)
-                    point2.background = getDrawable(R.drawable.point_style_stroke)
+                    point1.background = getDrawable(R.drawable.point_style_blue)
+                    point2.background = getDrawable(R.drawable.point_style_blue)
                     point3.background = getDrawable(R.drawable.point_style_blue)
                     point4.background = getDrawable(R.drawable.point_style_stroke)
                 }
                 4 -> {
-                    point1.background = getDrawable(R.drawable.point_style_stroke)
-                    point2.background = getDrawable(R.drawable.point_style_stroke)
-                    point3.background = getDrawable(R.drawable.point_style_stroke)
+                    point1.background = getDrawable(R.drawable.point_style_blue)
+                    point2.background = getDrawable(R.drawable.point_style_blue)
+                    point3.background = getDrawable(R.drawable.point_style_blue)
                     point4.background = getDrawable(R.drawable.point_style_blue)
                 }
             }

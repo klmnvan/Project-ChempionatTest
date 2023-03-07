8dff640549b99ba2c6ae3907f262df71ee4c2a1f
klmnvan <126891264+klmnvan@users.noreply.github.com>
2023-03-07 14:49:27 +0300

    До ввода пароля с помощью кружочков

--- app/src/main/java/com/example/chemp_podject/Input_and_register.kt
+++ app/src/main/java/com/example/chemp_podject/Input_and_register.kt
@@ -1,6 +1,7 @@
 package com.example.chemp_podject
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -17,20 +18,44 @@ class Input_and_register : AppCompatActivity() {
         setContentView(R.layout.activity_input_and_register)
         binding = ActivityInputAndRegisterBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
+        TextChanged()
+        init()
+    }
+    fun init()
+    {
+        binding!!.buttonDalee.setOnClickListener()
+        {
+            if(binding!!.inputEmailtext.text.isNotEmpty())
+            {
+                startActivity(Intent(this@Input_and_register,CodFromEmail::class.java))
+            }
+        }
 
+    }
+
+    fun TextChanged()
+    {
         binding!!.inputEmailtext.addTextChangedListener(object :TextWatcher{
             @SuppressLint("UseCompatLoadingForDrawables")
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int)
             {
-                if
-                binding!!.buttonDalee.background = this@Input_and_register.getDrawable(R.drawable.shape_button2)
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
             }
 
             override fun afterTextChanged(s: Editable?) {
+
+                if (binding!!.inputEmailtext.text.isNotEmpty())
+                {
+                    binding!!.buttonDalee.background = this@Input_and_register.getDrawable(R.drawable.shape_button2)
+                }
+                else
+                {
+                    binding!!.buttonDalee.background = this@Input_and_register.getDrawable(R.drawable.shape_button)
+                }
             }
         })
+
     }
 }

23a6891bd142dffdaa76f2a660a4f8be9f67c171
klmnvan <126891264+klmnvan@users.noreply.github.com>
2023-03-08 13:15:17 +0300

    Попытка наладить окно ввода пароля3
    Добавлен диапазон индексов + переменная password теперь и добавляет и удаляет символы, но не работает кнопка удаления

--- app/src/main/java/com/example/chemp_podject/CreatePassword.kt
+++ app/src/main/java/com/example/chemp_podject/CreatePassword.kt
@@ -17,60 +17,66 @@ class CreatePassword : AppCompatActivity() {
     }
     private fun init()
     {
-        with(binding!!)
-        {
-            buttonCircle1.setOnClickListener() {
-                password += buttonCircle1.text
-                index++
-                SetPassword(index)
+        if(index in 0..3) {
+            with(binding!!)
+            {
+                buttonCircle1.setOnClickListener() {
+                    password += buttonCircle1.text
+                    index++
+                    SetPassword(index)
+                }
+                buttonCircle2.setOnClickListener() {
+                    password += buttonCircle2.text
+                    index++
+                    SetPassword(index)
+                }
+                buttonCircle3.setOnClickListener() {
+                    password += buttonCircle3.text
+                    index++
+                    SetPassword(index)
+                }
+                buttonCircle4.setOnClickListener() {
+                    password += buttonCircle4.text
+                    index++
+                    SetPassword(index)
+                }
+                buttonCircle5.setOnClickListener() {
+                    password += buttonCircle5.text
+                    index++
+                    SetPassword(index)
+                }
+                buttonCircle6.setOnClickListener() {
+                    password += buttonCircle6.text
+                    index++
+                    SetPassword(index)
+                }
+                buttonCircle7.setOnClickListener() {
+                    password += buttonCircle7.text
+                    index++
+                    SetPassword(index)
+                }
+                buttonCircle8.setOnClickListener() {
+                    password += buttonCircle8.text
+                    index++
+                    SetPassword(index)
+                }
+                buttonCircle9.setOnClickListener() {
+                    password += buttonCircle9.text
+                    index++
+                    SetPassword(index)
+                }
+                buttonCircle0.setOnClickListener() {
+                    password += buttonCircle0.text
+                    index++
+                    SetPassword(index)
+                }
             }
-            buttonCircle2.setOnClickListener() {
-                password += buttonCircle2.text
-                index++
-                SetPassword(index)
-            }
-            buttonCircle3.setOnClickListener() {
-                password += buttonCircle3.text
-                index++
-                SetPassword(index)
-            }
-            buttonCircle4.setOnClickListener() {
-                password += buttonCircle4.text
-                index++
-                SetPassword(index)
-            }
-            buttonCircle5.setOnClickListener() {
-                password += buttonCircle5.text
-                index++
-                SetPassword(index)
-            }
-            buttonCircle6.setOnClickListener() {
-                password += buttonCircle6.text
-                index++
-                SetPassword(index)
-            }
-            buttonCircle7.setOnClickListener() {
-                password += buttonCircle7.text
-                index++
-                SetPassword(index)
-            }
-            buttonCircle8.setOnClickListener() {
-                password += buttonCircle8.text
-                index++
-                SetPassword(index)
-            }
-            buttonCircle9.setOnClickListener() {
-                password += buttonCircle9.text
-                index++
-                SetPassword(index)
-            }
-            buttonCircle0.setOnClickListener() {
-                password += buttonCircle0.text
-                index++
-                SetPassword(index)
-            }
-            buttonClean.setOnClickListener(){
+        }
+        if(index in 1..4) {
+            binding!!.buttonClean.setOnClickListener(){
                 //password = password.replace() //Как удалить символ из строки???
+                //password -= buttonCircle0.text
+                password = password?.dropLast(1)
                 index--
                 SetPassword(index)
             }
@@ -81,12 +87,18 @@ class CreatePassword : AppCompatActivity() {
         with(binding!!){
             when(point){
                 0 -> {
-                    point1.background = getDrawable(R.drawable.point_style_blue)
+                    point1.background = getDrawable(R.drawable.point_style_stroke)
                     point2.background = getDrawable(R.drawable.point_style_stroke)
                     point3.background = getDrawable(R.drawable.point_style_stroke)
                     point4.background = getDrawable(R.drawable.point_style_stroke)
                 }
                 1 -> {
+                    point1.background = getDrawable(R.drawable.point_style_blue)
+                    point2.background = getDrawable(R.drawable.point_style_stroke)
+                    point3.background = getDrawable(R.drawable.point_style_stroke)
+                    point4.background = getDrawable(R.drawable.point_style_stroke)
+                }
+                2 -> {
                     point1.background = getDrawable(R.drawable.point_style_blue)
                     point2.background = getDrawable(R.drawable.point_style_blue)
                     point3.background = getDrawable(R.drawable.point_style_stroke)

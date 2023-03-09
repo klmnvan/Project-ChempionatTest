3c341e78aa5ddb12499f256b2803ff1e671fb530
klmnvan <126891264+klmnvan@users.noreply.github.com>
2023-03-09 13:20:30 +0300

    Начало создания окна с заполнением карточки

---
+++ app/src/main/java/com/example/chemp_podject/CreateMap.kt
@@ -0,0 +1,11 @@
+package com.example.chemp_podject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateMap : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_map)
+    }
+}
